94ec09904034fdfcc9dd74eaf3fdeff6ace0b42d
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-23 17:43:04 +0330

    add alpha to the ScaleAnimator.kt add toScale var

--- numberview/src/main/java/com/mahdikh/vision/numberview/animator/ScaleAnimator.kt
+++ numberview/src/main/java/com/mahdikh/vision/numberview/animator/ScaleAnimator.kt
@@ -1,25 +1,45 @@
 package com.mahdikh.vision.numberview.animator
 
+import android.animation.ValueAnimator
 import android.graphics.Canvas
 import android.text.TextPaint
 
-class ScaleAnimator : PivotAnimator() {
+class ScaleAnimator : PivotAnimator {
+    private val alphaAnimator = ValueAnimator()
     private var scale = 1.0F
+    private var alpha = 255
+    var toScale: Float = 0.6F
+
+    constructor() : super()
+
+    constructor(toScale: Float) : super() {
+        this.toScale = toScale
+    }
+
+    override fun setDuration(duration: Long) {
+        super.setDuration(duration)
+        alphaAnimator.duration = duration
+    }
 
     override fun animate(newNumber: Int) {
-        setFloatValues(1.0F, 0.0F, 0.0F, 1.0F)
+        setFloatValues(1.0F, toScale, toScale, 1.0F)
+        alphaAnimator.setIntValues(255, 0, 0, 0, 255)
+        alphaAnimator.start()
         super.animate(newNumber)
     }
 
     override fun onDrawRightDigits(canvas: Canvas, paint: TextPaint) {
+        paint.alpha = alpha
         canvas.save()
         canvas.scale(scale, scale, getRightDigitCenterX(), getRightDigitCenterY())
         super.onDrawRightDigits(canvas, paint)
         canvas.restore()
+        paint.alpha = 255
     }
 
     override fun onAnimationUpdate(fraction: Float, animatedValue: Any) {
         scale = animatedValue as Float
+        alpha = alphaAnimator.animatedValue as Int
         super.onAnimationUpdate(fraction, animatedValue)
     }
 }
