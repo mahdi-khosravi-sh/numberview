58e64d736d139d422b4cd35295881c592afe0f88
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-23 21:34:44 +0330

    add axis to FlipAnimator.kt

--- numberview/src/main/java/com/mahdikh/vision/numberview/animator/FlipAnimator.kt
+++ numberview/src/main/java/com/mahdikh/vision/numberview/animator/FlipAnimator.kt
@@ -3,8 +3,18 @@ package com.mahdikh.vision.numberview.animator
 import android.graphics.Canvas
 import android.text.TextPaint
 
-class FlipAnimator : PivotAnimator() {
+const val AXIS_Y = 0
+const val AXIS_X = 1
+
+class FlipAnimator : PivotAnimator {
     private var scale = 1.0F
+    var axis: Int = AXIS_Y
+
+    constructor() : super()
+
+    constructor(axis: Int) : super() {
+        this.axis = axis
+    }
 
     override fun animate(newNumber: Int) {
         setFloatValues(1.0F, 0.0F, 0.0F, 1.0F)
@@ -13,7 +23,11 @@ class FlipAnimator : PivotAnimator() {
 
     override fun onDrawRightDigits(canvas: Canvas, paint: TextPaint) {
         canvas.save()
-        canvas.scale(scale, 1.0F, getRightDigitCenterX(), getRightDigitCenterY())
+        if (axis == AXIS_Y) {
+            canvas.scale(scale, 1.0F, getRightDigitCenterX(), getRightDigitCenterY())
+        } else {
+            canvas.scale(1.0F, scale, getRightDigitCenterX(), getRightDigitCenterY())
+        }
         super.onDrawRightDigits(canvas, paint)
         canvas.restore()
     }
