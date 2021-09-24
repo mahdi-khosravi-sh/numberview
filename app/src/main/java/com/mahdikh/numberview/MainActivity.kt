123f83cae728599a6224e33ec30b7e359231f518
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-24 14:18:43 +0330

    update activity

--- app/src/main/java/com/mahdikh/numberview/MainActivity.kt
+++ app/src/main/java/com/mahdikh/numberview/MainActivity.kt
@@ -2,14 +2,15 @@ package com.mahdikh.numberview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mahdikh.vision.numberview.animator.DefaultAnimator
 import com.mahdikh.vision.numberview.widget.NumberView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val numberView: NumberView = findViewById(R.id.numberView)
+
         numberView.setOnClickListener {
             numberView.increment()
         }
@@ -17,6 +18,5 @@ class MainActivity : AppCompatActivity() {
             numberView.decrement()
             true
         }
-        numberView.animator = DefaultAnimator()
     }
 }
